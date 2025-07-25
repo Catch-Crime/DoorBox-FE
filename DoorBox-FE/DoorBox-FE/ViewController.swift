372a37afe46ec187fd7c2f086b89c056a95aaa01
m1nzez <vivi020405@naver.com>
2025-07-25 13:21:45 +0900

    init : 프로젝트 초기세팅

---
+++ DoorBox-FE/DoorBox-FE/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  DoorBox-FE
+//
+//  Created by 김민지 on 7/25/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+
+}
+
---
+++ DoorBox-FE/DoorBox-FETests/DoorBox_FETests.swift
@@ -0,0 +1,17 @@
+//
+//  DoorBox_FETests.swift
+//  DoorBox-FETests
+//
+//  Created by 김민지 on 7/25/25.
+//
+
+import Testing
+@testable import DoorBox_FE
+
+struct DoorBox_FETests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
