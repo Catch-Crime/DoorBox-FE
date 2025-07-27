48bf6d6ef926a6e422fa2c668bab47c378533952
m1nzez <vivi020405@naver.com>
2025-07-27 10:11:18 +0900

    chore: 기본 패키지 추가

---
+++ DoorBox-FE/DoorBox-FE/Home/HomeView.swift
@@ -0,0 +1,8 @@
+//
+//  HomeView.swift
+//  DoorBox-FE
+//
+//  Created by 김민지 on 7/26/25.
+//
+
+import Foundation
---
+++ DoorBox-FE/DoorBox-FE/Login/LoginView.swift
@@ -0,0 +1,14 @@
+//
+//  LoginView.swift
+//  DoorBox-FE
+//
+//  Created by 김민지 on 7/27/25.
+//
+
+import Foundation
+import UIKit
+
+class LoginView: UIView {
+    
+    
+}
