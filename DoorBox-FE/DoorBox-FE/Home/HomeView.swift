f6cdcb2737797ac4e1837d7b65a0320f1510783e
m1nzez <vivi020405@naver.com>
2025-07-28 00:55:38 +0900

    design: 홈화면 middleView UI 구현(#1)

--- DoorBox-FE/DoorBox-FE/Home/HomeView.swift
+++ DoorBox-FE/DoorBox-FE/Home/HomeView.swift
@@ -50,11 +50,67 @@ class HomeView: UIView {
         button.isUserInteractionEnabled = true
     }
     
+    lazy var settingButton = UIButton().then { button in
+        button.setImage(UIImage(systemName: "gearshape.fill"), for: .normal)
+        button.isUserInteractionEnabled = true
+        button.tintColor = .blue02
+    }
+    
+    lazy var middleView = UIView()
+    
+    lazy var leftArrorwButton = UIButton().then { button in
+        button.setImage(UIImage(systemName: "chevron.left"), for: .normal)
+        button.isUserInteractionEnabled = true
+        button.tintColor = .blue02
+    }
+    
+    lazy var rightArrowButton = UIButton().then { button in
+        button.setImage(UIImage(systemName: "chevron.right"), for: .normal)
+        button.isUserInteractionEnabled = true
+        button.tintColor = .blue02
+    }
+    
+    lazy var dateStackView = UIStackView().then { stack in
+        stack.axis = .horizontal
+        stack.spacing = 10
+        stack.alignment = .center
+        stack.distribution = .equalCentering
+    }
+    
+    lazy var dateLabel = UILabel().then { label in
+        label.text = "12 / 12"
+        label.textColor = .blue02
+        label.font = .headline
+        label.textAlignment = .center
+    }
+    
+    lazy var currentDateLabel = UILabel().then { label in
+        label.text = "오늘"
+        label.backgroundColor = .blue02
+        label.font = .body2
+        label.textColor = .white
+        label.textAlignment = .center
+        label.layer.cornerRadius = 8
+        label.clipsToBounds = true
+    }
+    
+    lazy var middleLineView = UIView().then { view in
+        view.backgroundColor = .blue02
+    }
+    
+    lazy var photosTableView = UITableView().then { tableView in
+        tableView.backgroundColor = .grey01
+        tableView.register(PhotosTableViewCell.self, forCellReuseIdentifier: "PhotosTableViewCell")
+        tableView.isScrollEnabled = true
+    }
+    
+    
     private func addComponents() {
         addSubview(topView)
         topView.addSubview(profileButton)
         topView.addSubview(nicknameLabel)
         topView.addSubview(calendarButton)
+        topView.addSubview(settingButton)
         
         topView.snp.makeConstraints { make in
             make.top.equalToSuperview().offset(90)
@@ -62,12 +118,10 @@ class HomeView: UIView {
             make.height.equalTo(40)
         }
         
-        
         profileButton.snp.makeConstraints { make in
             make.top.leading.equalToSuperview()
             make.width.height.equalTo(40)
         }
-        
         
         nicknameLabel.snp.makeConstraints { make in
             make.centerY.equalTo(profileButton)
@@ -75,10 +129,65 @@ class HomeView: UIView {
         }
         
         calendarButton.snp.makeConstraints { make in
-            make.trailing.equalToSuperview()
-            make.width.height.equalTo(50)
+            make.trailing.equalTo(settingButton.snp.leading).offset(-10)
+            make.width.height.equalTo(40)
             make.centerY.equalTo(profileButton)
         }
+        
+        settingButton.snp.makeConstraints { make in
+            make.trailing.equalToSuperview()
+            make.width.height.equalTo(40)
+            make.centerY.equalTo(profileButton)
+        }
+        
+        addSubview(middleView)
+        middleView.addSubview(leftArrorwButton)
+        middleView.addSubview(dateStackView)
+        dateStackView.addArrangedSubview(dateLabel)
+        dateStackView.addArrangedSubview(currentDateLabel)
+        middleView.addSubview(rightArrowButton)
+        
+        middleView.snp.makeConstraints { make in
+            make.top.equalTo(topView.snp.bottom).offset(30)
+            make.leading.trailing.equalToSuperview().inset(30)
+            make.height.equalTo(40)
+        }
+        
+        leftArrorwButton.snp.makeConstraints { make in
+            make.leading.equalToSuperview()
+            make.width.height.equalTo(40)
+        }
+        
+        dateStackView.snp.makeConstraints { make in
+            make.centerY.equalToSuperview()
+            make.centerX.equalToSuperview()
+            make.height.equalTo(40)
+        }
+        
+        currentDateLabel.snp.makeConstraints { make in
+            make.width.equalTo(40)
+        }
+        
+        rightArrowButton.snp.makeConstraints { make in
+            make.trailing.equalToSuperview()
+            make.width.height.equalTo(40)
+        }
+        
+        addSubview(middleLineView)
+        middleLineView.snp.makeConstraints { make in
+            make.top.equalTo(middleView.snp.bottom).offset(10)
+            make.leading.trailing.equalToSuperview()
+            make.height.equalTo(1)
+        }
+        
+        addSubview(photosTableView)
+        photosTableView.snp.makeConstraints { make in
+            make.top.equalTo(middleLineView.snp.bottom).offset(30)
+            make.leading.trailing.equalToSuperview().inset(30)
+            make.bottom.equalToSuperview().inset(30)
+        }
+        
+        
     }
     
     
---
+++ DoorBox-FE/DoorBox-FE/Home/PhotosTableViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  PhotosTableViewCell.swift
+//  DoorBox-FE
+//
+//  Created by 김민지 on 7/28/25.
+//
+
+import UIKit
+import SnapKit
+
+class PhotosTableViewCell: UITableViewCell {
+    
+}
